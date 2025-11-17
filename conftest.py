e0afbca4bfafc01cfef9963e1d8b0f801f6c005f
Ankur123-Drax <ankurmaurya123.kkc@gmail.com>
2025-11-17 12:24:27 +0530

    crossBrowserImplementation

--- conftest.py
+++ conftest.py
@@ -1,9 +1,18 @@
 import pytest
 from utilities.base_driver import get_driver
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        help="Type of browser. Options: chrome, edge, mozila"
+    )
+
 @pytest.fixture
 def driver(request):
-    browser = request.c
+    browser = request.config.getoption("--browser")
     driver = get_driver()
     yield driver
     driver.quit()
+
